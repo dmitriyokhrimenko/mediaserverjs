c035fd6e0e303f5ebbf1d795f29c234e754a6d47
dm.okhrimenko <dm.okhrimenko@gmail.com>
2020-07-31 21:43:19 +0300

    add function to communicate

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+let users = {};
+
+const sendTo = (connection, message) => {
+    connection.send(JSON.stringify(message));
+};
+
+const sendToAll = (clients, type, { id, name: userName }) => {
+    Object.values(clients).forEach(client => {
+        if (client.name !== userName) {
+            client.send(
+                JSON.stringify({
+                    type,
+                    user: { id, userName }
+                })
+            );
+        }
+    });
+};
+
 wss.on("connection", ws => {
     ws.on("message", msg => {
         let data;
@@ -22,6 +41,41 @@ wss.on("connection", ws => {
         } catch (e) {
             console.log("Invalid JSON");
             data = {};
+        }
+        const { type, name } = data;
+        //Handle message by type
+        switch (type) {
+            //when a user tries to login
+            case "login":
+                //Check if username is available
+                if (users[name]) {
+                    sendTo(ws, {
+                        type: "login",
+                        success: false,
+                        message: "Username is unavailable"
+                    });
+                } else {
+                    const id = uuidv4();
+                    const loggedIn = Object.values(
+                        users
+                    ).map(({id, name: userName}) => ({id, userName}));
+                    users[name] = ws;
+                    ws.name = name;
+                    ws.id = id;
+                    sendTo(ws, {
+                        type: "login",
+                        success: true,
+                        users: loggedIn
+                    });
+                    sendToAll(users, "updateUsers", ws);
+                }
+                break;
+            default:
+                sendTo(ws, {
+                    type: "error",
+                    message: "Command not found: " + type
+                });
+                break;
         }
     });
     //send immediate a feedback to the incoming connection
